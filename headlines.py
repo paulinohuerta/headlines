7f60262d5f273aa81f03cfa3471595bbb2981b68
paulinohuerta <paulinohuerta@gmail.com>
2017-09-05 10:06:48 +0200

    more routing and adding hiperlinks to template

--- headlines.py
+++ headlines.py
@@ -4,10 +4,22 @@ from flask import render_template
 
 app= Flask(__name__)
 
-BBC_FEED = "http://feeds.bbci.co.uk/news/rss.xml"
+RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
+             'cnn':'http://rss.cnn.com/rss/edition.rss',
+             'fox':'http://feeds.foxnews.com/foxnews/latest',
+             'iol':'http://www.iol.co.za/cmlink/1.640'}
+
 @app.route("/")
-def get_news():
-  feed = feedparser.parse(BBC_FEED)
+@app.route("/bbc")
+def get_bbc():
+  return get_news('bbc')
+
+@app.route("/fox")
+def get_fox():
+  return get_news('fox')
+
+def get_news(publication):
+  feed = feedparser.parse(RSS_FEEDS[publication])
   return render_template("home.html", articles=feed['entries'])
 
 if __name__ == '__main__':
