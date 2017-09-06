c82c1afcfb42e7bb8439569788055d755521d41d
paulinohuerta <paulinohuerta@gmail.com>
2017-09-06 12:05:01 +0200

    getting weather by OpenWeatherMap, displaying data after parsing JSON

--- headlines.py
+++ headlines.py
@@ -2,6 +2,9 @@ from flask import Flask
 import feedparser
 from flask import render_template
 from flask import request
+import json
+import urllib
+import urllib2
 
 app= Flask(__name__)
 
@@ -18,7 +21,23 @@ def get_news():
   else:
          publication =  query.lower()
   feed = feedparser.parse(RSS_FEEDS[publication])
-  return render_template("home.html", articles=feed['entries'])
+  weather=get_weather("Seville,ES")
+  return render_template("home.html", articles=feed['entries'],weather=weather)
+
+def get_weather(query):
+  api_url="http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c390b245241307d0cec22b07f9dac57e"
+  query = urllib.quote(query)
+  url = api_url.format(query)
+  data = urllib2.urlopen(url).read()
+  parsed = json.loads(data)
+  weather = None
+  if parsed.get("weather"):
+   weather = {"description":
+   parsed["weather"][0]["description"],
+     "temperature":parsed["main"]["temp"],
+     "city":parsed["name"]
+   }
+   return weather
 
 if __name__ == '__main__':
   app.run(port=5300,debug=True)
