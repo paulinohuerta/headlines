f3d632968ebb117d8b6b1715d6b509185269af38
paulinohuerta <paulinohuerta@gmail.com>
2017-09-07 08:32:29 +0200

    customizing the city, adding another search box

--- headlines.py
+++ headlines.py
@@ -8,21 +8,34 @@ import urllib2
 
 app= Flask(__name__)
 
+DEFAULTS = {
+             'publication' : 'bbc',
+             'city' : 'London,UK'
+           }
 RSS_FEEDS = {'bbc':'http://feeds.bbci.co.uk/news/rss.xml',
              'cnn':'http://rss.cnn.com/rss/edition.rss',
              'fox':'http://feeds.foxnews.com/foxnews/latest',
              'iol':'http://www.iol.co.za/cmlink/1.640'}
 
 @app.route("/")
-def get_news():
-  query = request.args.get('publication')
+def home():
+  publication = request.args.get('publication')
+  if not publication:
+    publication = DEFAULTS['publication']
+  articles=get_news(publication)
+  city = request.args.get('city')
+  if not city:
+    city = DEFAULTS['city']
+  weather=get_weather(city)
+  return render_template("home.html", articles=articles,weather=weather)
+
+def get_news(query):
   if not query or query.lower() not in RSS_FEEDS:
-         publication = 'bbc'
+    query = DEFAULTS['publication']
   else:
-         publication =  query.lower()
+    publication =  query.lower()
   feed = feedparser.parse(RSS_FEEDS[publication])
-  weather=get_weather("Seville,ES")
-  return render_template("home.html", articles=feed['entries'],weather=weather)
+  return feed['entries']
 
 def get_weather(query):
   api_url="http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=c390b245241307d0cec22b07f9dac57e"
@@ -35,7 +48,8 @@ def get_weather(query):
    weather = {"description":
    parsed["weather"][0]["description"],
      "temperature":parsed["main"]["temp"],
-     "city":parsed["name"]
+     "city":parsed["name"],
+     "country":parsed["sys"]["country"]
    }
    return weather
 
